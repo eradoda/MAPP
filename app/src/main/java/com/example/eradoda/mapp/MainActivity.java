1458bc085f339b3773e2edd89a727f20eedbd3a2
eradoda <eradoda@ERAs-MacBook-Pro.local>
2015-07-14 16:36:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/eradoda/mapp/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.eradoda.mapp;
+
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends ActionBarActivity
+        implements NavigationDrawerCallbacks,OnMapReadyCallback {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private Toolbar mToolbar;
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Search");
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getFragmentManager().findFragmentById(R.id.fragment_drawer);
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setup(R.id.fragment_drawer, (DrawerLayout) findViewById(R.id.drawer), mToolbar);
+        // populate the navigation drawer
+        mNavigationDrawerFragment.setUserData("Username", "Email", BitmapFactory.decodeResource(getResources(), R.drawable.user_pic));
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        Toast.makeText(this, "Menu item selected -> " + position, Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mNavigationDrawerFragment.isDrawerOpen())
+            mNavigationDrawerFragment.closeDrawer();
+        else
+            super.onBackPressed();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void onMapReady(GoogleMap map) {
+        LatLng mannheim = new LatLng(49.487739, 8.466276);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(mannheim, 13));
+        UiSettings mapSettings;
+        mapSettings = map.getUiSettings();
+        mapSettings.setScrollGesturesEnabled(true);
+        mapSettings.setCompassEnabled(true);
+        mapSettings.setTiltGesturesEnabled(true);
+        mapSettings.setMyLocationButtonEnabled(true);
+        map.addMarker(new MarkerOptions()
+                        .title("Mannheim")
+                        .snippet("Welcome to Mannheim.").position(mannheim)
+        );
+    }
+
+
+}
---
+++ app/src/main/java/com/example/eradoda/mapp/NavigationDrawerCallbacks.java
@@ -0,0 +1,5 @@
+package com.example.eradoda.mapp;
+
+public interface NavigationDrawerCallbacks {
+    void onNavigationDrawerItemSelected(int position);
+}
